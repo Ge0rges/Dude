db7626bb8e66b1ef33686e5e8c316c2ad56b44e5
Georges Kanaan <ge0rges@ge0rges.com>
2017-01-26 19:18:36 +0200

    Updated LaunchScreen storyboard to iOS 7. Add Intents target.

---
+++ Dude Intents/IntentHandler.h
@@ -0,0 +1,13 @@
+//
+//  IntentHandler.h
+//  Dude Intents
+//
+//  Created by Georges Kanaan on 26/01/2017.
+//  Copyright © 2017 Georges Kanaan. All rights reserved.
+//
+
+#import <Intents/Intents.h>
+
+@interface IntentHandler : INExtension
+
+@end
---
+++ Dude IntentsUI/IntentViewController.h
@@ -0,0 +1,13 @@
+//
+//  IntentViewController.h
+//  Dude IntentsUI
+//
+//  Created by Georges Kanaan on 26/01/2017.
+//  Copyright © 2017 Georges Kanaan. All rights reserved.
+//
+
+#import <IntentsUI/IntentsUI.h>
+
+@interface IntentViewController : UIViewController <INUIHostedViewControlling>
+
+@end
